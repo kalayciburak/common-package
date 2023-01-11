69ff849075f771c782332b411d66ca493b149239
Burak KALAYCI <kalayciburak1996@gmail.com>
2023-01-11 22:19:31 +0300

    update pom.xml and delete unnecessary folder

--- src/main/java/com/kodlamaio/common/util/mapping/ModelMapperService.java
+++
@@ -1,8 +0,0 @@
-package com.kodlamaio.common.util.mapping;
-
-import org.modelmapper.ModelMapper;
-
-public interface ModelMapperService {
-    ModelMapper forResponse();
-    ModelMapper forRequest();
-}
