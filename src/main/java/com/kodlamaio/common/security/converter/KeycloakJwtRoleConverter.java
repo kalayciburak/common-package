ff5135cc1b9af06ca9cda2ffdc1a23109697567b
kalayciburak <kalayciburak1996@gmail.com>
2023-01-20 00:52:42 +0300

    add jwt role converter

---
+++ src/main/java/com/kodlamaio/common/security/converter/KeycloakJwtRoleConverter.java
@@ -0,0 +1,31 @@
+package com.kodlamaio.common.security.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class KeycloakJwtRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> claims = jwt.getClaims();
+        Map<String, Object> realmAccess = (Map<String, Object>) claims.get("realm_access");
+
+        if(realmAccess == null || realmAccess.isEmpty()) return new ArrayList<>();
+
+        List<String> roles = (List<String>) realmAccess.get("roles");
+        Collection<GrantedAuthority> result = new ArrayList<>();
+
+        final String ROLE_PREFIX = "ROLE_";
+        for (String roleName : roles){
+            result.add(new SimpleGrantedAuthority(ROLE_PREFIX + roleName));
+        }
+
+        return result;
+    }
+}
