df8a9db278b4a7dd9959d8e6d21f29345c0ce03d
kalayciburak <kalayciburak1996@gmail.com>
2023-01-27 23:20:56 +0300

    update package for production

--- src/main/java/com/kodlamaio/common/constants/Clients.java
+++ src/main/java/com/kodlamaio/common/constants/Clients.java
@@ -4,13 +4,18 @@ public class Clients {
     // Base
     public static final String BaseUrl = "http://localhost:9010/";
     public static final String BaseIdSuffix = "/{id}";
+    public static final String ProdInventoryName = "inventory-service";
+    public static final String ProdPaymentName = "payment-service";
 
     // Payment Client
     public static final String PaymentClient = "paymentclient";
     public static final String PaymentClientCheck = "payment-service/api/v1/payments/check";
+    public static final String PaymentClientCheckProd = "/api/v1/payments/check";
 
     // Car Client
     public static final String CarClient = "carclient";
     public static final String CarClientCheckAvailable = "inventory-service/api/v1/cars/check-car-available" + BaseIdSuffix;
     public static final String CarClientGetResponse = "inventory-service/api/v1/cars/get-car-response" + BaseIdSuffix;
+    public static final String CarClientCheckAvailableProd = "/api/v1/cars/check-car-available/" + BaseIdSuffix;
+    public static final String CarClientGetResponseProd = "/api/v1/cars/get-car-response/" + BaseIdSuffix;
 }
