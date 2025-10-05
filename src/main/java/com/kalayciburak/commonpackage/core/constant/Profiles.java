fc7422fbb19c3fe2cfe35f701d83dac38b5df843
kalayciburak <kalayciburak1996@gmail.com>
2025-10-05 17:06:13 +0300

    refactor(core): Profiles class'ına private constructor ekle
    - Utility class instantiation engellemesi
    - Code quality iyileştirmesi

--- src/main/java/com/kalayciburak/commonpackage/core/constant/Profiles.java
+++ src/main/java/com/kalayciburak/commonpackage/core/constant/Profiles.java
@@ -6,4 +6,6 @@ public final class Profiles {
     public static final List<String> DEVELOPMENT = List.of("dev", "development");
     public static final List<String> TEST = List.of("test", "testing");
     public static final List<String> PRODUCTION = List.of("prod", "preprod", "production");
+
+    private Profiles() {}
 }
